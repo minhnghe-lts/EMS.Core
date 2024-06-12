ff152e5576b84e5663d60cd809d0eb522c0aaeeb
Nghe Ngoc Minh <MinhNN@ltsgroup.tech>
2024-06-12 15:24:06 +0700

    fix db migration add entities

---
+++ Models/Entities/CourseSubject.cs
@@ -0,0 +1,12 @@
+﻿using EMS.Core.Commons;
+
+namespace EMS.Core.Models.Entities
+{
+    public class CourseSubject : BaseEntity
+    {
+        public long CourseId { get; set; }
+        public virtual Course Course { get; set; }
+        public long SubjectId { get; set; }
+        public virtual Subject Subject { get; set; }
+    }
+}
